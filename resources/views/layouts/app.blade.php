d89aadf06b2da343a3e33a2105db7fb0fdabbdde
Joshua Ohm <joshua.ohm@gmail.com>
2017-08-10 19:42:58 -0700

    Changed app name label to Time Tracker name in Laravel auth Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        Time Manager
+                        Time Tracker
                     </a>
                 </div>
 
